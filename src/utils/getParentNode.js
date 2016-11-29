bb56a84e4d4d8081018e75b43b879c9a01d4cbe7
Luke Batchelor <lbatchelor@atlassian.com>
2016-11-29 16:58:07 +1100

    fix(bug): fixes bug in getParentNode where anchor elements could return .host thinking it was an HTML element

--- src/utils/getParentNode.js
+++ src/utils/getParentNode.js
@@ -6,5 +6,12 @@
  * @returns {Element} parent
  */
 export default function getParentNode(element) {
-    return element.parentNode || element.host;
+    if (element.parentNode) {
+        return element.parentNode;
+    }
+    // we may be at a shadowDOM boundary, need to check .host but also need to make sure that .host
+    // is an element as anchors can also have a .host property
+    if (element.host && element.host instanceof HTMLElement) {
+        return element.host;
+    }
 }
